e9294e9b8362b01924d7d789657bf4f91a0c9434
Liza Brnk <liza.brnk@gmail.com>
2026-07-24 23:13:57 +0300

    fixed all issues in methods

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,11 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private Random rand = new Random();
+    private final Random rand = new Random();
 
     public String getRandomColor() {
-        int index = rand.nextInt(Colors.values().length);
-        Colors color = Colors.values()[index];
-        return color.name();
+        return Colors.values()[rand.nextInt(Colors.values().length)].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -4,18 +4,14 @@ import java.util.Random;
 
 public class Lottery {
     private static final int MAX_BALL_NUMBER = 100;
-    private Random rand = new Random();
-    private ColorSupplier colorSupplier = new ColorSupplier();
+
+    private final Random rand = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
-        int random = rand.nextInt(MAX_BALL_NUMBER);
-
         Ball ball = new Ball();
-        ball.setNumber(random);
-
-        String supply = colorSupplier.getRandomColor();
-        ball.setColor(supply);
-
+        ball.setNumber(rand.nextInt(MAX_BALL_NUMBER));
+        ball.setColor(colorSupplier.getRandomColor());
         return ball;
     }
 }
